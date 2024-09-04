85f2adf3311168d34b47cbcbf0c46ef98c339eda
jdang00 <jdang00@g.uafs.edu>
2024-09-04 18:06:23 -0500

    lesson 3 terms

--- src/lib/flashcards.ts
+++ src/lib/flashcards.ts
@@ -1,110 +1,579 @@
 export const flashcards = [
 	{
-		term: 'dermoid',
-		meaning: 'resembling skin'
+		term: 'Infrared',
+		meaning: 'Long wavelengths situated beneath red on the spectrum'
 	},
 	{
-		term: 'spectacle dermatitis',
-		meaning: 'skin inflammation from wearing spectacles'
+		term: 'Orbit',
+		meaning: 'The bony cavity containing the eyeball'
 	},
 	{
-		term: 'hemocyte',
-		meaning: 'any blood cell'
+		term: 'Supraorbital',
+		meaning: 'The forehead is ______ to the orbital'
 	},
 	{
-		term: 'vasiform',
-		meaning: 'vessel shaped'
+		term: 'Beneath',
+		meaning: 'If something is infraocular'
 	},
 	{
-		term: 'vasodilation',
-		meaning: 'expansion of vessels'
+		term: 'Superior',
+		meaning: 'Upper'
 	},
 	{
-		term: 'angionecrosis',
-		meaning: 'death of vessel (walls)'
+		term: 'Inferior',
+		meaning: 'Below'
 	},
 	{
-		term: 'angioneuroectomy',
-		meaning: 'excision of vessels and nerves'
+		term: 'Supernutrition',
+		meaning: 'Excessive nutirion'
 	},
 	{
-		term: 'osteoplasty',
-		meaning: 'surgical repair of bone surgery (plastic surgery)'
+		term: 'Epidermis',
+		meaning: 'The outermost portion of the skin'
 	},
 	{
-		term: 'arthrotomy',
-		meaning: 'surgical incision into joints'
+		term: 'Episcleritis',
+		meaning: 'Inflammation of the eye\u2019s episclera'
 	},
 	{
-		term: 'articular',
-		meaning: 'pertaining to a joint'
+		term: 'Epigastric',
+		meaning: 'Pertaining to the area above the stomach'
 	},
 	{
-		term: 'hepatoscopy',
-		meaning: 'examination of the liver'
+		term: 'Peri',
+		meaning: 'Prefix meaning around or surrounding'
 	},
 	{
-		term: 'otectomy',
-		meaning: 'excision of (middle) ear'
+		term: 'Periarteritis',
+		meaning: 'Inflammation of the tissue surrounding the heart'
 	},
 	{
-		term: 'cervical',
-		meaning: 'pertaining to the neck'
+		term: 'Perisoteum',
+		meaning: 'Tissue surrounding bone'
 	},
 	{
-		term: '-tom',
-		meaning: 'cutting'
+		term: 'Pericranium',
+		meaning: 'External periosteum of the skull'
 	},
 	{
-		term: 'esophagalgia',
-		meaning: 'pain in the esophagus'
+		term: 'Cranio',
+		meaning: 'Root word for skull'
 	},
 	{
-		term: 'arteriomotor',
-		meaning: 'pertaining to changes in the caliber of an artery'
+		term: 'Cradniology',
+		meaning: 'Study of the skull'
 	},
 	{
-		term: 'arteriectomy',
-		meaning: 'excision of all or part of an artery'
+		term: 'Craniocele',
+		meaning: 'Hernia of the skull'
 	},
 	{
-		term: 'phlebosis',
-		meaning: 'abnormal (noninflammatory) vein condition'
+		term: 'Intrahepatic',
+		meaning: 'Within the liver'
 	},
 	{
-		term: 'phlebosclerosis',
-		meaning: 'hardening of veins'
+		term: 'Intraocular',
+		meaning: 'Within the eye'
 	},
 	{
-		term: 'venography',
-		meaning: 'process of recording veins'
+		term: 'Incompetent',
+		meaning: 'Not competent'
 	},
 	{
-		term: 'venous',
-		meaning: 'having the quality of veins'
+		term: 'Insight',
+		meaning: 'To see into'
 	},
 	{
-		term: 'sclerometer',
-		meaning: 'instrument of measuring hardness'
+		term: 'Introspection',
+		meaning: 'Looking within'
 	},
 	{
-		term: 'necrotic tissue',
-		meaning: 'dead tissue'
+		term: 'Inspects',
+		meaning: 'Look into it closely'
 	},
 	{
-		term: 'necrectomy',
-		meaning: 'excision of dead tissue'
+		term: 'Interocular',
+		meaning: 'Between the eyes'
 	},
 	{
-		term: 'cutaneous',
-		meaning: 'having the quality of skin'
+		term: 'Interrenal',
+		meaning: 'Between the kidneys'
 	},
 	{
-		term: 'hepatocyte',
-		meaning: 'liver cell'
+		term: 'Renogastric',
+		meaning: 'Pertaining to the kidney and stomach'
 	},
 	{
-		term: 'oculofacial',
-		meaning: 'pertaining to the eyes and face'
+		term: 'Renopathy',
+		meaning: 'Kidney disease'
+	},
+	{
+		term: 'Intercostal',
+		meaning: 'Between the ribs'
+	},
+	{
+		term: 'Costicervical',
+		meaning: 'Pertaining to the ribs and neck'
+	},
+	{
+		term: 'Costectomy',
+		meaning: 'Surgical removal of a rib'
+	},
+	{
+		term: 'Costosuperior',
+		meaning: 'Pertaining to the upper ribs'
+	},
+	{
+		term: 'Precostal',
+		meaning: 'In front of the ribs'
+	},
+	{
+		term: 'Premature',
+		meaning: 'Before maturity'
+	},
+	{
+		term: 'Prodromal',
+		meaning: '_____ symptoms run before the onset of the disease'
+	},
+	{
+		term: 'Prognosis',
+		meaning: 'Foreknowledge'
+	},
+	{
+		term: 'Anteroom',
+		meaning: 'A room placed before or forming an entrance to another room'
+	},
+	{
+		term: 'Anteflexion',
+		meaning: 'Bend forward'
+	},
+	{
+		term: 'Dorsiflexion',
+		meaning: 'Bend backward'
+	},
+	{
+		term: 'Dorsospinal',
+		meaning: 'Pertaining to the back and spine'
+	},
+	{
+		term: 'Dorsalgia',
+		meaning: 'Pain in the back'
+	},
+	{
+		term: 'Dorsomedian',
+		meaning: 'Median line (spine) of the back'
+	},
+	{
+		term: 'Mesocornea',
+		meaning: 'The middle layer of the cornea'
+	},
+	{
+		term: 'Mesonasal',
+		meaning: 'Situated in the middle of the nose'
+	},
+	{
+		term: 'Mesocardium',
+		meaning: 'Middle of heart (wall)'
+	},
+	{
+		term: 'Parasagittal',
+		meaning: 'Beside the sagittal plane'
+	},
+	{
+		term: 'Parahepatic',
+		meaning: 'Beside the liver'
+	},
+	{
+		term: 'Parathyroid',
+		meaning: 'Beside the thyroid gland'
+	},
+	{
+		term: 'Parapsychology',
+		meaning: 'Beyond psychology'
+	},
+	{
+		term: 'Lateroposition',
+		meaning: 'Displacement to one side'
+	},
+	{
+		term: 'Unilateral',
+		meaning: 'A __________ ocular condition involves only one eye'
+	},
+	{
+		term: 'Auction',
+		meaning: 'Movement of one eye or of both eyes in opposite directions'
+	},
+	{
+		term: 'Supraduction',
+		meaning: 'Upward movement of one eye'
+	},
+	{
+		term: 'Infraduction',
+		meaning: 'Downward movement of one eye'
+	},
+	{
+		term: 'Lateroversion',
+		meaning: 'Turning the eyes to the side'
+	},
+	{
+		term: 'Infraversion',
+		meaning: 'Turning both eyes downward'
+	},
+	{
+		term: 'Supraversion',
+		meaning: 'Turning both eye upward'
+	},
+	{
+		term: 'Abduct',
+		meaning: 'To draw away from the midline'
+	},
+	{
+		term: 'Adduct',
+		meaning: 'To draw toward the midline'
+	},
+	{
+		term: 'Assimilation',
+		meaning:
+			'Occurs when the last consonant of a prefix changes when attached to certain root words (\u201cex to ec\u201d)'
+	},
+	{
+		term: 'Eccrine',
+		meaning: 'To separate out'
+	},
+	{
+		term: 'Excise',
+		meaning: 'To cut out'
+	},
+	{
+		term: 'Exhale',
+		meaning: 'To breathe out'
+	},
+	{
+		term: 'Inhale',
+		meaning: 'To breathe in'
+	},
+	{
+		term: 'Efferent',
+		meaning: 'Carrying away from center'
+	},
+	{
+		term: 'Afferent',
+		meaning: 'Carrying towards center'
+	},
+	{
+		term: 'Accomodate',
+		meaning: 'To adjust to'
+	},
+	{
+		term: 'Ectogenous',
+		meaning: 'Produced outside the organism'
+	},
+	{
+		term: 'Ectohormone',
+		meaning: 'Hormone secrete to the outside of the body'
+	},
+	{
+		term: 'Ectoderm',
+		meaning: 'Outside germ layer of the embryo'
+	},
+	{
+		term: 'Genophobia',
+		meaning: 'Fear of sex'
+	},
+	{
+		term: 'Hematogenic',
+		meaning: 'Blood'
+	},
+	{
+		term: 'Osteogenesis',
+		meaning: 'Bone formation'
+	},
+	{
+		term: 'Phonophobia',
+		meaning: 'Morbid fear of sounds of of speaking'
+	},
+	{
+		term: 'Hydrophobia',
+		meaning: 'Fear of water'
+	},
+	{
+		term: 'Anteromedial',
+		meaning: 'Situated in front and in the middle'
+	},
+	{
+		term: 'Anterolateral',
+		meaning: 'Situated in front and on the side'
+	},
+	{
+		term: 'Posteroinferior',
+		meaning: 'Situated behind and below'
+	},
+	{
+		term: 'Posteroanterior',
+		meaning: 'Back to front'
+	},
+	{
+		term: 'Caudal',
+		meaning: 'Positioned more toward the tail then a point of reference'
+	},
+	{
+		term: 'Cephalocaudal',
+		meaning: 'Pertaining to the long axis of the body from head to tail'
+	},
+	{
+		term: 'Caudad',
+		meaning: 'Toward the tail'
+	},
+	{
+		term: 'Cephalad',
+		meaning: 'Toward the head'
+	},
+	{
+		term: 'Cephalodynia',
+		meaning: 'Headache (dynia)'
+	},
+	{
+		term: 'Cephalalgia',
+		meaning: 'Headache (algia)'
+	},
+	{
+		term: 'Encephalo',
+		meaning: 'Inside the head'
+	},
+	{
+		term: 'Encephalocele',
+		meaning: 'Brain hernia'
+	},
+	{
+		term: 'Encephaloid',
+		meaning: 'Resembling the brain'
+	},
+	{
+		term: 'Ventral',
+		meaning: 'Pertaining to the belly'
+	},
+	{
+		term: 'Distal',
+		meaning: 'Remote'
+	},
+	{
+		term: 'Proximal',
+		meaning: 'Nearest'
+	},
+	{
+		term: 'Juxtaposition',
+		meaning: 'Positioned adjacent'
+	},
+	{
+		term: 'Juxtapyloric',
+		meaning: 'Situated near the pylorus'
+	},
+	{
+		term: 'Retroflextion',
+		meaning: 'Bend backward'
+	},
+	{
+		term: 'Retrocardiac',
+		meaning: 'Behind the heart'
+	},
+	{
+		term: 'Retroregression',
+		meaning: 'A \u201cbackward\u201d course of events'
+	},
+	{
+		term: 'Retroversion',
+		meaning: 'Turning backwards'
+	},
+	{
+		term: 'Endocranial',
+		meaning: 'Within the cranium'
+	},
+	{
+		term: 'Endophthalmitis',
+		meaning: 'Inflammation of the whole inside of the eye'
+	},
+	{
+		term: 'Entoptic',
+		meaning: 'Denotes visual phenomena which arise within the eye'
+	},
+	{
+		term: 'Orbitotomy',
+		meaning: 'Incision into the orbit'
+	},
+	{
+		term: 'Cranioplasty',
+		meaning: 'Surgical repair of the skull'
+	},
+	{
+		term: 'Inferolateral',
+		meaning: 'Situated below and to one side'
+	},
+	{
+		term: 'Superomedial',
+		meaning: 'Situated above the middle'
+	},
+	{
+		term: 'Supracostal',
+		meaning: 'Situated above the ribs'
+	},
+	{
+		term: 'Dorsolateral',
+		meaning: 'Pertaining to the back and side'
+	},
+	{
+		term: 'Infrarenal',
+		meaning: 'Below the kidney'
+	},
+	{
+		term: 'Perihepatic',
+		meaning: 'Around the liver'
+	},
+	{
+		term: 'Periocular',
+		meaning: 'Around the eye'
+	},
+	{
+		term: 'Antenatal',
+		meaning: 'Before birth'
+	},
+	{
+		term: 'Anteversion',
+		meaning: 'A forward turning'
+	},
+	{
+		term: 'Biorbital',
+		meaning: 'Pertaining to the two orbits'
+	},
+	{
+		term: 'Paraneural',
+		meaning: 'Beside a nerver'
+	},
+	{
+		term: 'Cryogenic',
+		meaning: 'Cell producing'
+	},
+	{
+		term: 'Anteroposterior',
+		meaning: 'From front to back of body'
+	},
+	{
+		term: 'Posterosuperior',
+		meaning: 'Situated behind and above'
+	},
+	{
+		term: 'Encephalitis',
+		meaning: 'Inflammation of the brain'
+	},
+	{
+		term: 'Dorsomesial',
+		meaning: 'Pertaining to the median line of the back'
+	},
+	{
+		term: 'Mediolateral',
+		meaning: 'Pertaining to the middle and to one side'
+	},
+	{
+		term: 'Exophthalmometer',
+		meaning: 'Instrument for measuring how much an eye protrudes from its socket'
+	},
+	{
+		term: 'Incise',
+		meaning: 'Cut in'
+	},
+	{
+		term: 'Retroocular',
+		meaning: 'Behind the eye'
+	},
+	{
+		term: 'Endoperiarteritis',
+		meaning: 'Inflammation of internal and external coats of an artery'
+	},
+	{
+		term: 'Orbital',
+		meaning: 'Pertaining to the orbit'
+	},
+	{
+		term: 'Craniosclerosis',
+		meaning: 'Hardening the thickening of skull bones'
+	},
+	{
+		term: 'Inferoposterior',
+		meaning: 'Situated below and behind'
+	},
+	{
+		term: 'Superolateral',
+		meaning: 'Situated above and to one side'
+	},
+	{
+		term: 'Infracostal',
+		meaning: 'Situated below the ribs'
+	},
+	{
+		term: 'Dorsal',
+		meaning: 'Pertaining to the back'
+	},
+	{
+		term: 'Epicranial',
+		meaning: 'Upon the skull'
+	},
+	{
+		term: 'Perineural',
+		meaning: 'Surrounding a nerve'
+	},
+	{
+		term: 'Intraorbital',
+		meaning: 'Within the orbit'
+	},
+	{
+		term: 'Interorbital',
+		meaning: 'Between the orbits'
+	},
+	{
+		term: 'Prerenal',
+		meaning: 'In front of the kidney'
+	},
+	{
+		term: 'Binocular',
+		meaning: 'Pertaining to the two eyes'
+	},
+	{
+		term: 'Abductor',
+		meaning: 'That which abducts'
+	},
+	{
+		term: 'Adductor',
+		meaning: 'That which draws toward the midline'
+	},
+	{
+		term: 'Inflection',
+		meaning: 'Bending inward'
+	},
+	{
+		term: 'Necrophobia',
+		meaning: 'Fear of death'
+	},
+	{
+		term: 'Thermogenic',
+		meaning: 'Producing heat'
+	},
+	{
+		term: 'Anterosuperior',
+		meaning: 'Situated in front and above'
+	},
+	{
+		term: 'Posterolateral',
+		meaning: 'Situated behind and to one side'
+	},
+	{
+		term: 'Encephalology',
+		meaning: 'Study of the brain'
+	},
+	{
+		term: 'Ventrad',
+		meaning: 'Toward the belly'
+	},
+	{
+		term: 'Dorsoventral',
+		meaning: 'Pertaining to the back and belly'
 	}
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { flashcards } from '$lib/flashcards';
 	import { onMount } from 'svelte';
+	import { Eye, ChevronLeft, ChevronRight } from 'lucide-svelte';
+	import { Confetti } from 'svelte-confetti';
 
 	export let ref: HTMLElement | null = null;
 	onMount(() => {
@@ -8,47 +11,115 @@
 		}
 	});
 
-	import { flashcards } from '$lib/flashcards'; // Import the flashcards
+	// Define flashcard structure with progress tracking
+	interface Flashcard {
+		term: string;
+		meaning: string;
+		status: 'done' | 'incomplete';
+	}
 
+	enum AnswerStatus {
+		empty,
+		correct,
+		incorrect
+	}
+
+	function shuffle(array: Flashcard[]): Flashcard[] {
+		for (let i = array.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+		return array;
+	}
+
+	let shuffledFlashcards: Flashcard[] = shuffle(
+		flashcards.map((card) => ({ ...card, status: 'incomplete' }))
+	);
 	let currentFlashcardIndex = 0;
 	let input: string = '';
-	let answer: string = flashcards[currentFlashcardIndex].term; // Set the initial answer
-	let meaning: string = flashcards[currentFlashcardIndex].meaning; // Set the initial meaning
-	let answerstatus: boolean = false;
+	let answer: string = shuffledFlashcards[currentFlashcardIndex].term;
+	let meaning: string = shuffledFlashcards[currentFlashcardIndex].meaning;
+	let answerstatus: AnswerStatus = AnswerStatus.empty;
+	let showAnswer: boolean = false;
 
+	// Check answer and move to the next card
 	function checkAnswer(): boolean {
 		if (input.trim().toLowerCase() === answer.trim().toLowerCase()) {
-			answerstatus = true;
-			setTimeout(nextFlashcard, 100); // Move to the next flashcard after a brief delay
+			answerstatus = AnswerStatus.correct;
+			shuffledFlashcards[currentFlashcardIndex].status = 'done';
+			setTimeout(nextFlashcard, 1000);
+
 			return true;
 		} else {
-			answerstatus = false;
-			console.log('FALSE');
+			answerstatus = AnswerStatus.incorrect;
+			if (ref) {
+				console.log(ref);
+				ref.focus();
+			}
 			return false;
 		}
 	}
 
 	function nextFlashcard() {
-		currentFlashcardIndex = (currentFlashcardIndex + 1) % flashcards.length;
-		answer = flashcards[currentFlashcardIndex].term;
-		meaning = flashcards[currentFlashcardIndex].meaning;
-		input = ''; // Clear the input for the next flashcard
-		answerstatus = false; // Reset the answer status
+		showAnswer = false;
+		currentFlashcardIndex = (currentFlashcardIndex + 1) % shuffledFlashcards.length;
+		answer = shuffledFlashcards[currentFlashcardIndex].term;
+		meaning = shuffledFlashcards[currentFlashcardIndex].meaning;
+		input = '';
+		answerstatus = AnswerStatus.empty;
+		if (ref) {
+			ref.focus();
+		}
+	}
+
+	function previousFlashcard() {
+		currentFlashcardIndex =
+			(currentFlashcardIndex - 1 + shuffledFlashcards.length) % shuffledFlashcards.length;
+		answer = shuffledFlashcards[currentFlashcardIndex].term;
+		meaning = shuffledFlashcards[currentFlashcardIndex].meaning;
+		input = '';
+		answerstatus = AnswerStatus.empty;
+		if (ref) {
+			ref.focus();
+		}
+	}
+
+	// Reset progress (optional feature)
+	function resetProgress() {
+		shuffledFlashcards = shuffle(
+			shuffledFlashcards.map((card) => ({ ...card, status: 'incomplete' }))
+		);
+		currentFlashcardIndex = 0;
+		nextFlashcard();
 	}
 </script>
 
 <div class="flex items-center justify-between w-full flex-col p-8 min-h-screen">
 	<div class="w-full max-w-3xl">
-		<h1 class="font-medium mt-3 text-3xl">Introduction to Optometry Terms</h1>
+		<div class="flex space-x-3 items-end">
+			<h1 class="font-medium mt-3 text-3xl">Introduction to Optometry Terms</h1>
+			<p class="font-bold text-gray-400">BETA</p>
+		</div>
+
 		<div class="border-b-2 border-gray-300 my-5"></div>
 		<p class="mt-3 text-xl">{meaning}</p>
 
 		<form>
-			{#if answerstatus === true}
+			{#if answerstatus === AnswerStatus.correct}
+				<Confetti />
+				<input
+					type="text"
+					placeholder="Type here"
+					class="input input-bordered input-success w-full max-w-xs mt-5"
+					bind:this={ref}
+					bind:value={input}
+				/>
+			{:else if answerstatus === AnswerStatus.empty}
 				<input
 					type="text"
 					placeholder="Type here"
 					class="input input-bordered w-full max-w-xs mt-5"
+					bind:this={ref}
 					bind:value={input}
 				/>
 			{:else}
@@ -56,12 +127,34 @@
 					type="text"
 					placeholder="Type here"
 					class="input input-bordered input-error w-full max-w-xs mt-5"
-					bind:value={input}
 					bind:this={ref}
+					bind:value={input}
 				/>
 			{/if}
-			<button type="submit" class="btn btn-primary mt-3" on:click={checkAnswer}>Enter</button>
+			<button type="submit" class="btn btn-primary mt-3 ms-3" on:click={checkAnswer}>Enter</button>
 		</form>
+
+		<div class="flex justify-center mt-5 space-x-4 items-center">
+			<button class="btn" on:click={() => previousFlashcard()}><ChevronLeft /></button>
+
+			<div class="card bg-base-100 w-96 shadow-md mt-6">
+				<div class="card-body">
+					{#if showAnswer === false}
+						<div class="flex justify-between">
+							<h2 class="card-title blur">{answer}</h2>
+							<button class="btn" on:click={() => (showAnswer = true)}><Eye /></button>
+						</div>
+					{:else}
+						<div class="flex justify-between">
+							<h2 class="card-title">{answer}</h2>
+							<button class="btn" on:click={() => (showAnswer = false)}><Eye /></button>
+						</div>
+					{/if}
+				</div>
+			</div>
+
+			<button class="btn" on:click={() => nextFlashcard()}><ChevronRight /></button>
+		</div>
 	</div>
 </div>
 
