9b90eb52146faacc80977b5da24b2b06fd3d10b5
jdang00 <jdang00@g.uafs.edu>
2024-09-04 19:13:45 -0500

    formatting changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,37 +102,41 @@
 		</div>
 
 		<div class="border-b-2 border-gray-300 my-5"></div>
-		<p class="mt-3 text-xl">{meaning}</p>
 
-		<form>
-			{#if answerstatus === AnswerStatus.correct}
-				<Confetti />
-				<input
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered input-success w-full max-w-xs mt-5"
-					bind:this={ref}
-					bind:value={input}
-				/>
-			{:else if answerstatus === AnswerStatus.empty}
-				<input
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered w-full max-w-xs mt-5"
-					bind:this={ref}
-					bind:value={input}
-				/>
-			{:else}
-				<input
-					type="text"
-					placeholder="Type here"
-					class="input input-bordered input-error w-full max-w-xs mt-5"
-					bind:this={ref}
-					bind:value={input}
-				/>
-			{/if}
-			<button type="submit" class="btn btn-primary mt-3 ms-3" on:click={checkAnswer}>Enter</button>
-		</form>
+		<div class="flex flex-col items-center">
+			<p class="mt-3 text-xl">{meaning}</p>
+
+			<form class="flex items-center">
+				{#if answerstatus === AnswerStatus.correct}
+					<Confetti />
+					<input
+						type="text"
+						placeholder="Type here"
+						class="input input-bordered input-success w-full max-w-lg mt-5"
+						bind:this={ref}
+						bind:value={input}
+					/>
+				{:else if answerstatus === AnswerStatus.empty}
+					<input
+						type="text"
+						placeholder="Type here"
+						class="input input-bordered w-full max-w-xs mt-5"
+						bind:this={ref}
+						bind:value={input}
+					/>
+				{:else}
+					<input
+						type="text"
+						placeholder="Type here"
+						class="input input-bordered input-error w-full max-w-xs mt-5"
+						bind:this={ref}
+						bind:value={input}
+					/>
+				{/if}
+				<button type="submit" class="btn btn-primary ms-3 mt-5" on:click={checkAnswer}>Enter</button
+				>
+			</form>
+		</div>
 
 		<div class="flex justify-center mt-5 space-x-4 items-center">
 			<button class="btn" on:click={() => previousFlashcard()}><ChevronLeft /></button>
@@ -140,20 +144,20 @@
 			<div class="card bg-base-100 w-96 shadow-md mt-6">
 				<div class="card-body">
 					{#if showAnswer === false}
-						<div class="flex justify-between">
+						<div class="flex">
 							<h2 class="card-title blur">{answer}</h2>
-							<button class="btn" on:click={() => (showAnswer = true)}><Eye /></button>
 						</div>
+						<button class="btn mt-5" on:click={() => (showAnswer = true)}><Eye /></button>
 					{:else}
-						<div class="flex justify-between">
+						<div class="flex">
 							<h2 class="card-title">{answer}</h2>
-							<button class="btn" on:click={() => (showAnswer = false)}><Eye /></button>
 						</div>
+						<button class="btn mt-5" on:click={() => (showAnswer = false)}><Eye /></button>
 					{/if}
 				</div>
 			</div>
 
-			<button class="btn" on:click={() => nextFlashcard()}><ChevronRight /></button>
+			<button class="btn mt-5" on:click={() => nextFlashcard()}><ChevronRight /></button>
 		</div>
 	</div>
 </div>
