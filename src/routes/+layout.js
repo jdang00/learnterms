6db8538a3b38d987b5b3a764801dc9c8c18d4b89
jdang00 <jdang00@g.uafs.edu>
2024-09-23 09:57:21 -0500

    posthog anaylytics

---
+++ src/routes/+layout.js
@@ -0,0 +1,12 @@
+import posthog from 'posthog-js';
+import { browser } from '$app/environment';
+
+export const load = async () => {
+	if (browser) {
+		posthog.init('phc_3eXFYO1aHVEWM75fi3wXFE6OiJZiDNvI5pcl67S19fK', {
+			api_host: 'https://us.i.posthog.com',
+			person_profiles: 'always'
+		});
+	}
+	return;
+};
