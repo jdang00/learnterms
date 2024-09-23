9ae6ecf11bc392daa532371aeda316cf49ed6df1
jdang00 <jdang00@g.uafs.edu>
2024-09-22 22:33:02 -0500

    user auth stuff

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -12,6 +12,7 @@ type Flashcard = {
 
 export const load = async ({ locals }) => {
 	const user = locals.session;
+	let userid = '';
 
 	const { data: allFlashcards, error: flashcardsError } = await supabase
 		.from(PRIVATE_FLASHCARD_TABLE)
@@ -23,16 +24,42 @@ export const load = async ({ locals }) => {
 		throw error(500, 'Error fetching flashcards');
 	}
 
-	let userid = '';
+	// Calling users table twice. Considering fixing to call once and get data
 	if (user) {
-		const { data: userID } = await supabase
+		const { data: foundUser, error: userError } = await supabase
 			.from(PRIVATE_USER_TABLE)
-			.select('id')
-			.eq('clerk_user_id', user.userId);
+			.select()
+			.eq('clerk_user_id', user.userId)
+			.single();
 
-		const userIdValue = userID?.[0]?.id || null;
-		userid = userIdValue;
+		if (userError && userError.code !== 'PGRST116') {
+			console.error('Error checking for existing user:', userError);
+			throw error(500, 'Error checking user status');
+		}
+		if (!foundUser) {
+			const { error: insertError } = await supabase
+				.from(PRIVATE_USER_TABLE)
+				.insert({ clerk_user_id: user.userId, name: user.claims.userName })
+				.select()
+				.single();
+
+			if (insertError) {
+				console.error('Error creating new user:', insertError);
+				throw error(500, 'Error creating new user');
+			}
+		}
+
+		if (user) {
+			const { data: userID } = await supabase
+				.from(PRIVATE_USER_TABLE)
+				.select('id')
+				.eq('clerk_user_id', user.userId);
+
+			const userIdValue = userID?.[0]?.id || null;
+			userid = userIdValue;
+		}
 	}
+
 	return {
 		flashcards: allFlashcards ?? [],
 		user: userid
