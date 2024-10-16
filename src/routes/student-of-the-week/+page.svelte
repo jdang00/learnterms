a2ef69056b77db313853f83a8c279b3c4734e2ff
jdang00 <jdang00@g.uafs.edu>
2024-10-16 08:40:17 -0500

    hbd court

--- src/routes/student-of-the-week/+page.svelte
+++ src/routes/student-of-the-week/+page.svelte
@@ -3,7 +3,27 @@
 	import { Confetti } from 'svelte-confetti';
 </script>
 
-<Confetti />
+<div
+	style="
+ position: fixed;
+ top: -50px;
+ left: 0;
+ height: 100vh;
+ width: 100vw;
+ display: flex;
+ justify-content: center;
+ overflow: hidden;
+ pointer-events: none;"
+>
+	<Confetti
+		x={[-5, 5]}
+		y={[0, 0.1]}
+		delay={[500, 2000]}
+		duration="2000"
+		amount="200"
+		fallDistance="100vh"
+	/>
+</div>
 <div class="flex flex-col items-center mt-5">
 	<h1 class="text-4xl font-bold mt-5">Student of the Week</h1>
 	<h2 class="text-gray-400">(changes every month)</h2>
