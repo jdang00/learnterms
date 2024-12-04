2875ce43dc073979edfc8cce0fc88d1aa1fd9672
jdang00 <jdang00@g.uafs.edu>
2024-12-04 16:48:19 -0600

    Ping pong edits

--- src/routes/student-of-the-week/+page.svelte
+++ src/routes/student-of-the-week/+page.svelte
@@ -24,14 +24,16 @@
 	/>
 </div>
 <div class="flex flex-col items-center mt-5">
-	<h1 class="text-4xl font-bold mt-5">2024 NSUOCO Division 1 Ping Pong Tournament CHAMPION</h1>
+	<h1 class="text-4xl font-bold mt-5 text-center md: px-12">
+		2024 NSUOCO Division 1 Ping Pong Tournament CHAMPION
+	</h1>
 	<h2 class="text-gray-400">December 4, 2024</h2>
 
 	<div class="flex flex-col md:flex-row md:space-x-16 justify-center items-center mt-12">
 		<img
 			class="object-contain h-96 mb-8"
 			alt="Champion"
-			src={'https://utfs.io/f/DYlXFqnaImOreuJb2ZbxHBVn21bZq7P8ofUF4rlcphAwxv3y'}
+			src={'https://utfs.io/f/DYlXFqnaImOrYyQ64xfUyAfWjLu9FCobHl7mOgI8YXVr0KZx'}
 		/>
 		<div class="self-start">
 			<h2 class="text-4xl mb-3 font-medium">Courtney Swift</h2>
@@ -49,6 +51,6 @@
 
 <style>
 	ul {
-		list-style-type: '✨ ';
+		list-style-type: '🏓 ';
 	}
 </style>
