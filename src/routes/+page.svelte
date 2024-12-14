b8e2a16945bc4c2ad410451426bfa0107bdbdc2e
jdang00 <jdang00@g.uafs.edu>
2024-12-14 13:13:34 -0600

    Revert "more formatting"
    This reverts commit 5e3afbea401c4a3f80449026b384059d4dfd4f0c.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 <!--Hi mom-->
 <div class="flex flex-col space-y-2 items-center text-center">
-	<h1 class="font-bold text-4xl mt-24 mb-3 mx-12">
+	<h1 class="font-bold text-4xl mt-24 mb-3">
 		Big things are coming to <span class="text-primary">LearnTerms!</span>
 	</h1>
 	<p class="mx-16 text-base-content">
