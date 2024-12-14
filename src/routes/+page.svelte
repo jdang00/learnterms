51f457714617cd62e7b24c23b40438e269282b26
jdang00 <jdang00@g.uafs.edu>
2024-12-13 18:01:30 -0600

    under construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import Quiz from './quiz/quiz.svelte';
-
-	export let data;
 </script>
 
 <!--Hi mom-->
-<div class="flex flex-col space-y-2 items-center">
-	<Quiz {data} />
+<div class="flex flex-col space-y-2 items-center text-center">
+	<h1 class="font-bold text-4xl mt-24 mb-3">
+		Big things are coming to <span class="text-primary">LearnTerms!</span>
+	</h1>
+	<p class="mx-16 text-base-content">
+		We’re on a little holiday break, just like you (probably). While you’re enjoying some holiday
+		cheer, we’re decking the site with upgrades and improvements.
+	</p>
+
+	<p class=" mt-12 mx-16 text-base-content">
+		Check back soon—your next study session is going to be amazing.
+	</p>
 </div>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,8 +4,8 @@
 	<a href="/blog/LearnTerms 1.0" class="card lg:card-side bg-base-100 hover:bg-base-200">
 		<div class="card-body">
 			<h2 class="card-title">LearnTerms 1.0 Release</h2>
-			<p class="text-neutral">September 23, 2024</p>
-			<p class="text-neutral">
+			<p class="text-base-content">September 23, 2024</p>
+			<p class="text-base-content">
 				I’m thrilled to announce that LearnTerms has officially reached its 1.0 production release!
 				What began as a personal study tool for mastering the 100+ definitions we had to learn
 				weekly in Introduction to Optometry has grown into a full-fledged quizzing platform.
@@ -15,8 +15,8 @@
 	<a href="/blog/LENS" class="card lg:card-side bg-base-100 hover:bg-base-200">
 		<div class="card-body">
 			<h2 class="card-title">The LENS Algorithm: Playing the Numbers Game</h2>
-			<p class="text-neutral">October 16, 2024</p>
-			<p class="text-neutral">
+			<p class="text-base-content">October 16, 2024</p>
+			<p class="text-base-content">
 				I’m thrilled to announce that LearnTerms has officially reached its 1.0 production release!
 				What began as a personal study tool for mastering the 100+ definitions we had to learn
 				weekly in Introduction to Optometry has grown into a full-fledged quizzing platform.
