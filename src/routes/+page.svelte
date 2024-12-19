7f928851feb0a97aa063508842e576262244278d
jdang00 <jdang00@g.uafs.edu>
2024-12-18 22:30:19 -0600

    annoying title fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 
 <!--Hi mom-->
 <div class="flex flex-col space-y-2 items-center text-center">
-	<h1 class="font-bold text-4xl mt-24 mb-3 relative">
+	<h1 class="font-bold text-4xl mt-24 mb-3 relative mx-12">
 		Big things are coming to <span class="text-primary relative">
 			<a href="/mod">LearnTerms</a>
 			<span
