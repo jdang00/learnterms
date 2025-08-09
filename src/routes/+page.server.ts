bcdc6fd259387a86766119972558796e858b61e4
jdang00 <justindang02@gmail.com>
2025-08-09 06:28:16 -0500

    merge

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,7 @@ import { redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const { userId } = locals.auth();
+	console.log(userId);
 
 	if (userId) {
 		return redirect(307, '/classes');
